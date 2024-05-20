f42d4eb895597bb5d883332096abd96c6ae3d309
joao966 <joaooliveira966@gmail.com>
2024-05-20 18:56:26 -0300

    FIX: disable cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,7 @@ import { configService } from './config/config.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  // app.enableCors();
 
   if (!configService.isProduction()) {
     const config = new DocumentBuilder()
